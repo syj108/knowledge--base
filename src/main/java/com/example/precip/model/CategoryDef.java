419f0b2b49464dfdffb60ec979623fbd3bd39f9d
songlan <songlan.syj@alibaba-inc.com>
2026-05-25 17:58:08 +0800

    清空分支错误内容

--- src/main/java/com/example/precip/model/CategoryDef.java
+++
@@ -1,6 +0,0 @@
-package com.example.precip.model;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record CategoryDef(String key, String name, String description) {}
